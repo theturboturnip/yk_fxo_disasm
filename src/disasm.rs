17006df7ce037a953e6164b70a8deafb637c0002
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-12-30 14:13:52 +0000

    Use new scalar/vector structs when disassembling

--- src/disasm.rs
+++ src/disasm.rs
@@ -1,10 +1,10 @@
 use std::{collections::{HashSet, HashMap}, thread::current, panic::PanicInfo};
 
 use turnip_gfx_disasm::{
-    abstract_machine::{analysis::{dependency::ScalarDependencies, variable::disassemble}, VMName, display::DisplayVec, vector::VectorOf},
-    amdil_text::{AMDILError, AMDILDecoder, AMDILProgram},
+    abstract_machine::{analysis::{dependency::ScalarDependencies, variable::disassemble}, VMName, display::DisplayVec, expr::{HLSLVector, HLSLScalar, Reg}},
+    amdil_text::{AMDILErrorContext, AMDILDecoder, AMDILProgram},
     // rdna2::{vm::RDNA2DataRef, RDNA2DecodeError, RDNA2Decoder, RDNA2Program},
-    Decoder, Program, hlsl::{compat::{HLSLCompatibleAbstractVM, program_to_hlsl}, display::DWrap, kinds::{HLSLKindBitmask, HLSLKind}, HLSLScalar, HLSLVector, vm::HLSLAbstractVM}
+    Decoder, Program, hlsl::{compat::{HLSLCompatibleAbstractVM, program_to_hlsl}, display::DWrap, kinds::{HLSLKindBitmask, HLSLKind}, vm::HLSLAbstractVM}
 };
 
 // pub fn disassemble_rdna2(rdna2: &[u8]) -> Result<RDNA2Program, RDNA2DecodeError> {
@@ -24,31 +24,35 @@ pub fn print_output_depedencies<T: HLSLCompatibleAbstractVM>(program: &impl Prog
         println!("\t{:?}", r);
     }
 
-    for dependent in resolver.discard_dependencies() {
-        println!("discard depends on {}", DWrap(dependent))
+    for dependent in resolver.discard_dependencies {
+        println!("discard depends on {}", DWrap(&(HLSLScalar::Component(dependent.0, dependent.1), dependent.2)))
     }
 
-    let mut out_deps: Vec<_> = resolver.dependents().into_iter().filter_map(|(out, deps)| {
-        if out.is_output() {
+    let mut out_deps: Vec<_> = resolver.dependents.iter().filter_map(|(out, deps)| {
+        if out.0.is_output() {
             let mut v = deps.iter().collect::<Vec<_>>();
             v.sort();
-            let mut lits = vec![];
             let mut vecs = vec![];
             let mut current_vec = None;
 
             for s in v {
                 match s {
-                    (HLSLScalar::Component(reg, _), _) => {
+                    (reg, _, _) => {
                         match &mut current_vec {
                             None => {
                                 current_vec = Some((reg, vec![s.clone()]))
                             }
                             Some((other_reg, other_scalars)) if reg != *other_reg => {
-                                let kind = other_scalars[0].1;
-                                vecs.push((
-                                    HLSLVector::new(&(other_scalars.iter().map(|(x, _)| x.clone()).collect::<Vec<_>>())).unwrap(),
-                                    kind
-                                ));
+                                let kind = other_scalars[0].2;
+                                vecs.push(
+                                    HLSLVector::of_scalars(
+                                        other_scalars
+                                            .iter()
+                                            .map(|(reg, comp, _)| HLSLScalar::Component(reg.clone(), *comp))
+                                            .collect(),
+                                        kind
+                                    )
+                                );
                                 current_vec = Some((reg, vec![s.clone()]))
                             }
                             Some((_this_reg, sibling_scalars)) => {
@@ -56,15 +60,22 @@ pub fn print_output_depedencies<T: HLSLCompatibleAbstractVM>(program: &impl Prog
                             }
                         }
                     },
-                    (HLSLScalar::Literal(_), _) => lits.push(s),
                 }
             }
 
             if let Some((reg, scalars)) = current_vec {
-                vecs.push((HLSLVector::new(&(scalars.iter().map(|(x, _)| x.clone()).collect::<Vec<_>>())).unwrap(), reg.toplevel_kind()))
+                vecs.push(
+                    HLSLVector::of_scalars(
+                        scalars
+                            .iter()
+                            .map(|(reg, comp, _)| HLSLScalar::Component(reg.clone(), *comp))
+                            .collect(),
+                        reg.output_kind()
+                    )
+                );
             }
             Some(
-                (out, lits, vecs)
+                (out, vecs)
             )
         } else {
             None
@@ -72,11 +83,11 @@ pub fn print_output_depedencies<T: HLSLCompatibleAbstractVM>(program: &impl Prog
     }).collect();
     out_deps.sort_by(|(out1, ..), (out2, ..)| out1.partial_cmp(out2).unwrap());
 
-    for (out, lits, vecs) in out_deps {
-        println!("{} depends on {}; {}", 
-            DWrap((out, HLSLKindBitmask::all().into())),
-            DisplayVec::Sep { vec: &(vecs.iter().map(|v| DWrap(v)).collect()), sep: ", " },
-            DisplayVec::Sep { vec: &(lits.into_iter().map(|l| DWrap(l))).collect(), sep: ", "}
+    for (out, vecs) in out_deps {
+        println!("{} depends on {}", 
+            DWrap(&(HLSLScalar::Component(out.0.clone(), out.1), HLSLKindBitmask::all().into())),
+            DisplayVec::Sep { vec: &(vecs.iter().map(|v| DWrap((v, v.output_kind()))).collect()), sep: ", " },
+            // DisplayVec::Sep { vec: &(lits.into_iter().map(|l| DWrap(l))).collect(), sep: ", "}
         )
     }
 
@@ -86,7 +97,7 @@ pub fn print_output_depedencies<T: HLSLCompatibleAbstractVM>(program: &impl Prog
     }
 }
 
-pub fn disassemble_amdil_text(amdil_text: &[u8]) -> Result<AMDILProgram, AMDILError> {
+pub fn disassemble_amdil_text(amdil_text: &[u8]) -> Result<AMDILProgram, AMDILErrorContext> {
     let amdil_text = std::str::from_utf8(amdil_text).expect("text was invalid utf8");
     AMDILDecoder::new().decode(amdil_text)
 }
